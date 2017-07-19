d90a2cadcd02847e42e64bd4b89e6d7aaeeefa6e
ffon <poty1900@hotmail.com>
2017-07-19 12:00:19 +0700

    Update getName.php

--- getName.php
+++ getName.php
@@ -7,7 +7,7 @@
     $content = file_get_contents('php://input');
     $arrJson = json_decode($content, true);
     
-    $strUrl = "https://api.line.me/v2/bot/profile/Ub5fea2ff169cba24b2179fd33e59e454";
+    $strUrl = "https://api.line.me/v2/bot/profile/U7de80d0a2ceea863e831375badd2eb55";
     
     $header = array(
     'Content-Type: application/json',
