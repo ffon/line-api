eb8ab05ea5786e836477155a29f2af7e5582a623
ffon <poty1900@hotmail.com>
2017-07-19 16:59:42 +0700

    Update getName.php

--- getName.php
+++ getName.php
@@ -28,7 +28,7 @@
     var_dump($result);
 
     echo "result decode "."<br>";
-    $result_decode = (array)json_decode($result);
+    $result_decode = (array)json_encode($result);
     var_dump($result_decode);
     
 //     $name = $result_decode->displayName;
