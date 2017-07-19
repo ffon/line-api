49eef955f64ccbcb5d2fc8966011cf2b6d34ae4d
ffon <poty1900@hotmail.com>
2017-07-19 18:05:57 +0700

    Update reply_getMid_tb_ffon3_ft.php

--- reply_getMid_tb_ffon3_ft.php
+++ reply_getMid_tb_ffon3_ft.php
@@ -21,10 +21,26 @@ function getMid()
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
     $arrPostData['messages'][0]['type'] = "text";
-    $arrPostData['messages'][0]['text'] = "ID = ".$arrJson['events'][0]['source']['userId'];
+    //$arrPostData['messages'][0]['text'] = "ID = ".$arrJson['events'][0]['source']['userId'];
     $mid = $arrJson['events'][0]['source']['userId'];
 
     getName($mid);
+    
+     if ($arrJson['events'][0]['message']['text'] == "สวัสดี") {
+        $arrPostData = array();
+        $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+        $arrPostData['messages'][0]['type'] = "text";
+        $arrPostData['messages'][0]['text'] = "สวัสดี ";
+ 
+     }else{
+        $arrPostData = array();
+        $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+        $arrPostData['messages'][0]['type'] = "text";
+        $arrPostData['messages'][0]['text'] = "สวัสดี";
+       
+         
+     }
+    
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $strUrl);
