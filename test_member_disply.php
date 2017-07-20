5fc615227d5bbea8df180ae5b3db7864f3d21cc4
ffon <poty1900@hotmail.com>
2017-07-20 15:41:36 +0700

    Add files via upload

---
+++ test_member_disply.php
@@ -0,0 +1,16 @@
+<html>
+    <?php
+        $chAdd = curl_init();
+        curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/get_line_member');
+        curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
+        "Content-Type: application/json",
+                                )
+        );
+        $result = curl_exec($chAdd);
+        $err    = curl_error($chAdd);
+        curl_close($chAdd);
+        var_dump($result);
+    ?>
+</html>
