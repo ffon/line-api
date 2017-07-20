04353d326326e471e501bf2a70577146742795b0
ffon <poty1900@hotmail.com>
2017-07-20 22:37:23 +0700

    Update push_form.php

--- push_form.php
+++ push_form.php
@@ -78,7 +78,7 @@
                                             
                                             <?php
                                             for($i=0;$i<$count;$i++){ ?>
-                                              echo $i;
+                                              <?php echo $i; ?>
                                             <div class="checkbox">
                                                 <label><input type="checkbox" id="<?php echo $de[$i]->user_id; ?>" name="<?php echo mid[$i] ?>"> <?php echo $de[$i]->member_name; echo "  "; echo $de[$i]->user_id; ?></label>
                                             </div>
