1232dc4b311390e12a50c096d34a5e1024526178
ffon <poty1900@hotmail.com>
2017-07-20 19:02:00 +0700

    Update push_fix.php

--- push_fix.php
+++ push_fix.php
@@ -31,7 +31,7 @@
             </div>
           
             <div class="col-md-8 col-md-offset-2">
-                <form method="post">
+                <form action="push_fix.php" method="post">
                     <div class="form-group">
 <!--                         <label>Line@</label><br>
                             <input type="radio"  class="btn btn-primary"  value="QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=" name="tokenLine"> Line@ffon</input>
@@ -87,41 +87,7 @@
 
 
 
-<?php
 
-$strAccessToken = '3Wv1vcrB1uJCUf4D+nqgA8mcjtSTPYCbe5ZpR4LgyoMKb764ZewaWwAtn3kqRZLFcFvVkJH2cMox8g/ml2Ulw7YGORdDhgVXJvKZs24dnQoqaMfbpRNftFepCpuS+Hw/TdH7gkctEgiYIj4ot5A3hQdB04t89/1O/w1cDnyilFU=';
- $text = $_POST['textArea'];
-$midUser = $_POST['mid'];  
-
-foreach($midUser as $key => $mid){        
-        $messages = [
-            "type" => "text",
-            "text" => $text
-        ];
- 
-        $post_data = [
-            "to" => $mid,
-            "messages" => [$messages]
-        ];
- 
-        $header = array(
-            'Content-Type: application/json',
-            'Authorization: Bearer ' . $strAccessToken
-        );
-        $url = 'https://api.line.me/v2/bot/message/push';
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-
-        $result = curl_exec($ch);
-        curl_close($ch);
-}
- 
- 
- ?>
 </body>
 
 </html>
