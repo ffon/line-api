56d092b90cc993013bf5aaff456f57f2b22a9f59
ffon <poty1900@hotmail.com>
2017-07-20 18:44:14 +0700

    Update push.php

--- push.php
+++ push.php
@@ -33,50 +33,6 @@
             <div class="col-md-8 col-md-offset-2">
                 <form method="post">
                     <div class="form-group">
-<!--                         <label>Line@</label><br>
-                            <input type="radio"  class="btn btn-primary"  value="QQ4FDBydERg5R34tFiff7M+OOuRNzYKDA/btJh4Whsgl0ztKiDparY2v3TyaoL1LQPMU/R+dN8JPUEl4UZ3VdcnPVwB3VGFVHPu6HhvSBctP74gTqe5/G/kLHS2Ixe3w0jsLIaN0guHlHI+3q9c9ZQdB04t89/1O/w1cDnyilFU=" name="tokenLine"> Line@ffon</input>
-                            <input type="radio"  class="btn btn-primary"  value="xV/huVeGtwzqkP96ryoZdb3X0BHoAyuIXaXlIbf2axHa+CTebqsx8np2B8jQGVhnm31zNpHaY6lIWJ0LRzIqnxsgrBt0a+dKb56qqBmOlDttf2ciCpLUM4jXevfZFg1pqEJjUsahi4On8qIg1ocUWgdB04t89/1O/w1cDnyilFU=" name="tokenLine"> Line@oil</input>
-                         <br> -->
-                        <label>Text</label>
-                        <textarea class="form-control" rows="8" id="textArea" name="textArea"></textarea>
-                    </div>
-                    
-                    <!--buttonMember-->
-<!DOCTYPE html>
-<html lang="th">
- 
-<head>
-    <title>Push Messages</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <style>
-        .head-form h1 {
-            padding-top: 30px;
-            padding-bottom: 50px;
-        }
-        #myModal {
-            margin-top: 100px;
-        }
-        .button-sc .button {
-            margin-bottom: 30px;
-            margin-top: 20px;
-        }
-    </style>
-</head>
-<body>
-
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 head-form">
-                <h1 align = "center">Push Massages</h1>
-            </div>
-          
-            <div class="col-md-8 col-md-offset-2">
-                <form method="post">
-                    <div class="form-group">
                         <label>Text</label>
                         <textarea class="form-control" rows="8" id="textArea" name="textArea"></textarea>
                     </div>
