ac883df791b60b6b50cd8af3a3e0cfb4f1a59cba
ffon <poty1900@hotmail.com>
2017-07-20 18:38:17 +0700

    Update test2_disply_member.php

--- test2_disply_member.php
+++ test2_disply_member.php
@@ -43,7 +43,8 @@
                         MEMBER
                         </button>
                     </div>
-                    <!--Modal-->
+                    
+                 <!--Modal-->
                     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
                         <form method="post">
                             <div class="modal-dialog" role="document">
@@ -105,7 +106,39 @@
             $('#myInput').focus()
         })
     </script>
-
+<?php
+$strAccessToken = '3Wv1vcrB1uJCUf4D+nqgA8mcjtSTPYCbe5ZpR4LgyoMKb764ZewaWwAtn3kqRZLFcFvVkJH2cMox8g/ml2Ulw7YGORdDhgVXJvKZs24dnQoqaMfbpRNftFepCpuS+Hw/TdH7gkctEgiYIj4ot5A3hQdB04t89/1O/w1cDnyilFU=';
+ $text = $_POST['textArea'];
+$midUser = $_POST['mid'];  
+  
+foreach($midUser as $key => $mid){        
+        $messages = [
+            "type" => "text",
+            "text" => $text
+        ];
+ 
+        $post_data = [
+            "to" => $mid,
+            "messages" => [$messages]
+        ];
+ 
+        $header = array(
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $strAccessToken
+        );
+        $url = 'https://api.line.me/v2/bot/message/push';
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        $result = curl_exec($ch);
+        curl_close($ch);
+}
+ 
+ 
+ ?>
     
     </body>
 </html>
