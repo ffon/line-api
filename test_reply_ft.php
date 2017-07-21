f5f7a80dcc80792a6c0e230b4e4de330b06fd6b2
ffon <poty1900@hotmail.com>
2017-07-21 16:28:02 +0700

    Update test_reply_ft.php

--- test_reply_ft.php
+++ test_reply_ft.php
@@ -19,7 +19,7 @@ function getMid()
         $arrPostData['messages'][0]['text'] = "สวัสดี ID = ".$arrJson['events'][0]['source']['userId'];
         $mid = $arrJson['events'][0]['source']['userId'];
         getName($mid);
-   // }
+ //   }
 //     else if ($arrJson['events'][0]['message']['text'] == "สวัสดี") {
 //         $arrPostData = array();
 //         $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
@@ -39,7 +39,7 @@ function getMid()
     curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $arrPostData);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
