d80b92e429e8d6cf4fcc776db0a999a6a3e0ec7d
ffon <poty1900@hotmail.com>
2017-07-21 16:36:16 +0700

    Update test_reply_ft.php

--- test_reply_ft.php
+++ test_reply_ft.php
@@ -12,14 +12,14 @@ function getMid()
     $arrHeader[] = "Content-Type: application/json";
     $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
     
-    //if($arrJson['events'][0]['message']['follow']){
+    if($arrJson['events'][0]['type']['follow']){
         $arrPostData = array();
         $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
         $arrPostData['messages'][0]['type'] = "follow";
         $arrPostData['messages'][0]['text'] = "สวัสดี ID = ".$arrJson['events'][0]['source']['userId'];
         $mid = $arrJson['events'][0]['source']['userId'];
         getName($mid);
- //   }
+    }
 //     else if ($arrJson['events'][0]['message']['text'] == "สวัสดี") {
 //         $arrPostData = array();
 //         $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
