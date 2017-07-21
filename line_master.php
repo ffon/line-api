0338aa51a7d8c10310ace1311f96be758cc3f259
ffon <poty1900@hotmail.com>
2017-07-21 17:32:53 +0700

    Create line_master.php

---
+++ line_master.php
@@ -0,0 +1,18 @@
+<?php
+
+    $name = isset($_POST['name']) ? $_POST['name'] : 0;
+    $token = isset($_POST['token']) ? $_POST['token'] : 0;
+
+    $chAdd = curl_init();
+    curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/line_master?line_name='.$name.'&access_token='.$token.'&add_by=1');
+    curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+                            )
+    );
+    $result = curl_exec($chAdd);
+    $err    = curl_error($chAdd);
+    curl_close($chAdd);
+    
+?>
