3a11cc9282e5ea3503a6b0aba73be999b75692a5
ffon <poty1900@hotmail.com>
2017-07-23 23:39:32 +0700

    Create push_msg_selcet_no_fix_token.php.php

---
+++ push_msg_selcet_no_fix_token.php.php
@@ -0,0 +1,13 @@
+<?php
+
+$token = $_GET['token_line_mas'];
+$text = $_GET['textArea'];
+$midUser = $_GET['mid']; 
+$strAccessToken = '3Wv1vcrB1uJCUf4D+nqgA8mcjtSTPYCbe5ZpR4LgyoMKb764ZewaWwAtn3kqRZLFcFvVkJH2cMox8g/ml2Ulw7YGORdDhgVXJvKZs24dnQoqaMfbpRNftFepCpuS+Hw/TdH7gkctEgiYIj4ot5A3hQdB04t89/1O/w1cDnyilFU=';
+echo 'token';echo "<br>";
+var_dump($token);
+echo "Mid User ";echo "<br>";
+var_dump($midUser);
+
+ 
+ ?>
