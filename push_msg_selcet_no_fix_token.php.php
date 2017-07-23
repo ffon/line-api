b4f4221864c7abbde9782d8184667dc499aae8b3
ffon <poty1900@hotmail.com>
2017-07-24 00:30:37 +0700

    Update push_msg_selcet_no_fix_token.php.php

--- push_msg_selcet_no_fix_token.php.php
+++ push_msg_selcet_no_fix_token.php.php
@@ -4,13 +4,13 @@ $token = $_GET['token_line_mas'];
 $text = $_GET['textArea'];
 $midUser = $_GET['mid']; 
 
-// $strAccessToken = $token;
-// echo 'token';echo "<br>";
-// var_dump($token);
-// echo 'strAccessToken';echo "<br>";
-// var_dump($strAccessToken);
-// echo "Mid User ";echo "<br>";
-// var_dump($midUser);
+$strAccessToken = $token;
+echo 'token';echo "<br>";
+var_dump($token);
+echo 'strAccessToken';echo "<br>";
+var_dump($strAccessToken);
+echo "Mid User ";echo "<br>";
+var_dump($midUser);
 
 // foreach($midUser as $key => $mid){        
 //         $messages = [
