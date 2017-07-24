568f392b17d4741750f0681f6c0dc32035161934
ffon <poty1900@hotmail.com>
2017-07-24 15:06:44 +0700

    Update check_event_follow_v2.php

--- check_event_follow_v2.php
+++ check_event_follow_v2.php
@@ -10,7 +10,7 @@
     
     $arrPostData = array();
     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-    $arrPostData['messages'][0]['type'] = "follow";
+    $arrPostData['messages'][0]['type'] = "text";
     $arrPostData['messages'][0]['text'] = "สวัสดี ID = ".$arrJson['events'][0]['source']['userId'];
     $mid = $arrJson['events'][0]['source']['userId'];
     
