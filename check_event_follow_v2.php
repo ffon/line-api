681f2fcdadc15e1eac1516d1f7019fb823324234
ffon <poty1900@hotmail.com>
2017-07-24 14:22:09 +0700

    Create check_event_follow_v2.php

---
+++ check_event_follow_v2.php
@@ -0,0 +1,45 @@
+<?php
+    
+    $strAccessToken = "3Wv1vcrB1uJCUf4D+nqgA8mcjtSTPYCbe5ZpR4LgyoMKb764ZewaWwAtn3kqRZLFcFvVkJH2cMox8g/ml2Ulw7YGORdDhgVXJvKZs24dnQoqaMfbpRNftFepCpuS+Hw/TdH7gkctEgiYIj4ot5A3hQdB04t89/1O/w1cDnyilFU=";
+    $content = file_get_contents('php://input');
+    $arrJson = json_decode($content, true);
+    $strUrl = "https://api.line.me/v2/bot/message/reply";
+    $arrHeader = array();
+    $arrHeader[] = "Content-Type: application/json";
+    $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+    
+//     $arrPostData = array();
+//     $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+//     $arrPostData['messages'][0]['type'] = "follow";
+//     $arrPostData['messages'][0]['text'] = "สวัสดี ID = ".$arrJson['events'][0]['source']['userId'];
+//     $mid = $arrJson['events'][0]['source']['userId'];
+    
+    
+if ($arrJson['events'][0]['event']['follow']) {
+        $arrPostData = array();
+        $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+        $arrPostData['events'][0]['type'] = "follow";
+        $arrPostData['messages'][0]['text'] = "สวัสดี ID = ".$arrJson['events'][0]['source']['userId'];
+        //getName($mid);
+ 
+     }else{
+    
+        $arrPostData = array();
+        $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+        $arrPostData['messages'][0]['type'] = "text";
+        $arrPostData['messages'][0]['text'] = $arrJson['events'][0]['follow'];         
+     }
+   
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $strUrl);
+    curl_setopt($ch, CURLOPT_HEADER, false);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $arrPostData);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($ch);
+    curl_close ($ch);
+    var_dump($result);
+?>
