3a9d21e720076f7054890985a1c844d1e8cfa5f5
ffon <poty1900@hotmail.com>
2017-07-24 17:05:33 +0700

    Update test_getMethod_form.php

--- test_getMethod_form.php
+++ test_getMethod_form.php
@@ -1,6 +1,6 @@
 <html>
   <form action="test_getMethod.php" method="GET">
-    <input value="3Wv1vcrB1uJCUf4D+nqgA8mcjtSTPYCbe5ZpR4LgyoMKb764ZewaWwAtn3kqRZLFcFvVkJH2cMox8g/ml2Ulw7YGORdDhgVXJvKZs24dnQoqaMfbpRNftFepCpuS+Hw/TdH7gkctEgiYIj4ot5A3hQdB04t89/1O/w1cDnyilFU=" name="token">
-    <input type="submit" name="submit">
+    <input type="text" value="3Wv1vcrB1uJCUf4D+nqgA8mcjtSTPYCbe5ZpR4LgyoMKb764ZewaWwAtn3kqRZLFcFvVkJH2cMox8g/ml2Ulw7YGORdDhgVXJvKZs24dnQoqaMfbpRNftFepCpuS+Hw/TdH7gkctEgiYIj4ot5A3hQdB04t89/1O/w1cDnyilFU=" name="token">
+    <button type="submit">Submit</button>
   </form>
 </html>
