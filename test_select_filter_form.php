d0b0b2203d9b7a926edcf5979ac615e2a51a193e
ffon <poty1900@hotmail.com>
2017-07-26 00:34:28 +0700

    Update test_select_filter_form.php

--- test_select_filter_form.php
+++ test_select_filter_form.php
@@ -19,13 +19,15 @@
   
 ?>
   <form action="test_select_member_filter.php" method="GET">  
-  <?php $i=0; while($i!=$count_line_mas){ ?>
+  
   <select class="ui fluid search dropdown" multiple="">
-    <option value="">Choes line@</option>
-    <option value="<?php echo $de_line_mas->id; ?>" ><?php echo $de_line_mas[i]->line_name ?></option>
-  </select>
-  <?php $i++; ?>
+    <option value="">Choose line@</option>
+    <?php $i=0; while($i!=$count_line_mas){ ?>
+    <option value="<?php echo $de_line_mas->id; ?>" ><?php echo $de_line_mas[i]->line_name ?></option> 
+    <?php $i++; ?>
   <?}?>
+  </select>
+ 
     <button type="submit" name="submit">
   </form>
 
