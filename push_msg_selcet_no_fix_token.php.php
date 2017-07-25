2b94791f6f72e7dc6a671b5ffbc3a1e60663c193
ffon <poty1900@hotmail.com>
2017-07-25 13:21:08 +0700

    Update push_msg_selcet_no_fix_token.php.php

--- push_msg_selcet_no_fix_token.php.php
+++ push_msg_selcet_no_fix_token.php.php
@@ -3,7 +3,6 @@
 $token = $_GET['token_line_mas'];
 $text = $_GET['textArea'];
 $midUser = $_GET['mid']; 
-$strAccessTokens = '3Wv1vcrB1uJCUf4D+nqgA8mcjtSTPYCbe5ZpR4LgyoMKb764ZewaWwAtn3kqRZLFcFvVkJH2cMox8g/ml2Ulw7YGORdDhgVXJvKZs24dnQoqaMfbpRNftFepCpuS+Hw/TdH7gkctEgiYIj4ot5A3hQdB04t89/1O/w1cDnyilFU=';
 
 
 $strAccessToken = $token;
