99e816fe39f5849e7aecc0018b9585281f7de9ab
ffon <poty1900@hotmail.com>
2017-07-25 13:04:05 +0700

    Update test_form_push2222.php

--- test_form_push2222.php
+++ test_form_push2222.php
@@ -110,7 +110,7 @@
                                             for($i=0;$i<$count;$i++){ ?>
                                             <div class="checkbox">
                                                
-                                                <label><input type="checkbox" value="<?php echo $de[$i]->user_id; ?>" name="mid[]"> <?php echo $de[$i]->member_name; echo "  "; echo $de[$i]->line_master_id; ?></label>
+                                                <label><input type="checkbox" value="<?php echo $de[$i]->user_id; ?>" name="mid[]"> <?php echo $de[$i]->member_name; echo "  "; echo $de[$i]->line_master_id; echo $de_line_mas[$i]->id; ?></label>
                                             </div>
                                            <?php }?>
                                         </div>
