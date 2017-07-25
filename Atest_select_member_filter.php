d0f262bc4c6fd332417eb78e293a57b82897da17
ffon <poty1900@hotmail.com>
2017-07-26 00:55:42 +0700

    Update Atest_select_member_filter.php

--- Atest_select_member_filter.php
+++ Atest_select_member_filter.php
@@ -1,9 +1,9 @@
 <html>
 <?php 
     $line_id = $_GET['id_line_master'];
-    $access_token = $_GET['access_token'];
+    //$access_token = $_GET['access_token'];
 
     var_dump($line_id);
-    var_dump($access_token);
+    //var_dump($access_token);
 ?>
 </html>
