86a158de13113ca9d12af322e157db9279e881b4
ffon <poty1900@hotmail.com>
2017-07-26 01:47:16 +0700

    Create A_push_msg.php

---
+++ A_push_msg.php
@@ -0,0 +1,10 @@
+<?php
+$token = $_GET['token_line_mas'];
+$text = $_GET['textArea'];
+$midUser = $_GET['mid'];
+
+var_dump($token);echo "<br>";
+var_dump($text);echo "<br>";
+var_dump($midUser);
+
+?>
