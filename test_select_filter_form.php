085f9132ac981f1b0e978451a3b3ee873880efe3
ffon <poty1900@hotmail.com>
2017-07-26 00:31:24 +0700

    Update test_select_filter_form.php

--- test_select_filter_form.php
+++ test_select_filter_form.php
@@ -14,71 +14,19 @@
 
   $de_line_mas = json_decode($result);
   $count_line_mas = count($de_line_mas);
+
   
   
 ?>
-  
-<select class="ui fluid search dropdown" multiple="">
-  <option value="">State</option>
-  <option value="AL">Alabama</option>
-  <option value="AK">Alaska</option>
-  <option value="AZ">Arizona</option>
-  <option value="AR">Arkansas</option>
-  <option value="CA">California</option>
-  <option value="CO">Colorado</option>
-  <option value="CT">Connecticut</option>
-  <option value="DE">Delaware</option>
-  <option value="DC">District Of Columbia</option>
-  <option value="FL">Florida</option>
-  <option value="GA">Georgia</option>
-  <option value="HI">Hawaii</option>
-  <option value="ID">Idaho</option>
-  <option value="IL">Illinois</option>
-  <option value="IN">Indiana</option>
-  <option value="IA">Iowa</option>
-  <option value="KS">Kansas</option>
-  <option value="KY">Kentucky</option>
-  <option value="LA">Louisiana</option>
-  <option value="ME">Maine</option>
-  <option value="MD">Maryland</option>
-  <option value="MA">Massachusetts</option>
-  <option value="MI">Michigan</option>
-  <option value="MN">Minnesota</option>
-  <option value="MS">Mississippi</option>
-  <option value="MO">Missouri</option>
-  <option value="MT">Montana</option>
-  <option value="NE">Nebraska</option>
-  <option value="NV">Nevada</option>
-  <option value="NH">New Hampshire</option>
-  <option value="NJ">New Jersey</option>
-  <option value="NM">New Mexico</option>
-  <option value="NY">New York</option>
-  <option value="NC">North Carolina</option>
-  <option value="ND">North Dakota</option>
-  <option value="OH">Ohio</option>
-  <option value="OK">Oklahoma</option>
-  <option value="OR">Oregon</option>
-  <option value="PA">Pennsylvania</option>
-  <option value="RI">Rhode Island</option>
-  <option value="SC">South Carolina</option>
-  <option value="SD">South Dakota</option>
-  <option value="TN">Tennessee</option>
-  <option value="TX">Texas</option>
-  <option value="UT">Utah</option>
-  <option value="VT">Vermont</option>
-  <option value="VA">Virginia</option>
-  <option value="WA">Washington</option>
-  <option value="WV">West Virginia</option>
-  <option value="WI">Wisconsin</option>
-  <option value="WY">Wyoming</option>
-</select>
-  <script>
-    $('#keep-order').multiSelect({ keepOrder: true });
-    $('#multi-select')
-  .dropdown()
-;
-    $('#search-select')
-  .dropdown()
-;
-  </script>
+  <form action="test_select_member_filter.php" method="GET">  
+  <?php $i=0; while($i!=$count_line_mas){ ?>
+  <select class="ui fluid search dropdown" multiple="">
+    <option value="">Choes line@</option>
+    <option value="<?php echo $de_line_mas->id; ?>" ><?php echo $de_line_mas[i]->line_name ?></option>
+  </select>
+  <?php $i++; ?>
+  <?}?>
+    <button type="submit" name="submit">
+  </form>
+
 <html>
