d8c178e964721754ca9e9f01272f777016682b22
ffon <poty1900@hotmail.com>
2017-07-25 11:13:57 +0700

    Update Line_master.php

--- Line_master.php
+++ Line_master.php
@@ -8,9 +8,10 @@
     $en_token = json_encode($token);
     echo "token en"."<br>";
     var_dump($en_token);
+    $url = 'http://uat.dxplace.com/dxtms/line_master?line_name='.$name.'&access_token='.$en_token.'&add_by=1';
 
     $chAdd = curl_init();
-    curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/line_master?line_name='.$name.'&access_token='.$en_token.'&add_by=1');
+    curl_setopt($chAdd, CURLOPT_URL, urlencode($url));
     curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
     curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
