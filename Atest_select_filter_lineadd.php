f13d603cd3502eb11cd031aeb373c7c127492e1c
ffon <poty1900@hotmail.com>
2017-07-26 14:23:29 +0700

    Update Atest_select_filter_lineadd.php

--- Atest_select_filter_lineadd.php
+++ Atest_select_filter_lineadd.php
@@ -1,21 +1,22 @@
 <html>
-  0
+  00
 <?php 
   $chAdd = curl_init();
   curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/get_line_master');
   curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
   curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($chAdd, CURLOPT_HTTPHEADER, araray(
-  "Content-Type: application/json",
-                          )
-  );
+  "Content-Type: application/json"
+));
   $result = curl_exec($chAdd);
   $err    = curl_error($chAdd);
   curl_close($chAdd);
 
   $de_line_mas = json_decode($result);
   $count_line_mas = count($de_line_mas);
- 
+
+  
+  
 ?>
   <form action="Atest_select_member_filter.php" method="GET">  
   
@@ -28,7 +29,6 @@
      // echo $de_line_mas[$id_line]->access_token;
       }?>
     </select>
-    
     <button type="submit" name="submit">submit</button>
   </form>
 
