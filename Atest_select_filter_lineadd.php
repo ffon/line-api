9f89ee589970de09831f0802179a5d3cc639c391
ffon <poty1900@hotmail.com>
2017-07-26 13:47:18 +0700

    Update Atest_select_filter_lineadd.php

--- Atest_select_filter_lineadd.php
+++ Atest_select_filter_lineadd.php
@@ -25,36 +25,6 @@
       <option value="">Choose line@</option>
       <?php $i=0; while($i!=$count_line_mas){ ?>
         <option type="text"  value="<?php $id_line=$de_line_mas[$i]->id; echo $id_line;  ?>"> <?php echo $id_line; echo " "; echo $de_line_mas[$i]->line_name ?></option> 
-           <input type="hidden" value="<?php echo $de_line_mas[$id_line]->access_token ?>" name="access_token" /> 
-      <?php $i++; ?>
-      <?}?>
-    </select>a<html>
-  1
-<?php 
-  $chAdd = curl_init();
-  curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/get_line_master');
-  curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
-  curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
-  "Content-Type: application/json",
-                          )
-  );
-  $result = curl_exec($chAdd);
-  $err    = curl_error($chAdd);
-  curl_close($chAdd);
-
-  $de_line_mas = json_decode($result);
-  $count_line_mas = count($de_line_mas);
-
-  
-  
-?>
-  <form action="Atest_select_member_filter.php" method="GET">  
-  
-    <select  name="id_line_master">
-      <option value="">Choose line@</option>
-      <?php $i=0; while($i!=$count_line_mas){ ?>
-        <option type="text"  value="<?php $id_line=$de_line_mas[$i]->id; echo $id_line;  ?>"> <?php echo $id_line; echo " "; echo $de_line_mas[$i]->line_name ?></option> 
           <?php if($id_line==$de_line_mas[$i]->id){?> 
          <input type="hidden" value="<?php echo $de_line_mas[$id_line]->access_token ?>" name="access_token" /> 
       
@@ -62,13 +32,6 @@
       $i++;
       }?>
     </select>
-    
-    <button type="submit" name="submit">submit</button>
-  </form>
-
-
-<html>
-
     
     <button type="submit" name="submit">submit</button>
   </form>
