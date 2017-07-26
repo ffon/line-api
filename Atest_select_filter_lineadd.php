877ff67132aaed2f2a6bf0c9c5a31fc12c296d87
ffon <poty1900@hotmail.com>
2017-07-26 14:59:09 +0700

    Update Atest_select_filter_lineadd.php

--- Atest_select_filter_lineadd.php
+++ Atest_select_filter_lineadd.php
@@ -2,20 +2,21 @@
   4
 <?php 
   $chAdd = curl_init();
-  curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/get_line_master');
-  curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
-  curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($chAdd, CURLOPT_HTTPHEADER, araray(
-  "Content-Type: application/json",
-));
-  $result = curl_exec($chAdd);
-  $err    = curl_error($chAdd);
-  curl_close($chAdd);
-  $de_line_mas = json_decode($result);
-  $count_line_mas = count($de_line_mas);
+    curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/get_line_master');
+    curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($chAdd, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($chAdd, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+                 )
+    );
+    $result = curl_exec($chAdd);
+    $err    = curl_error($chAdd);
+    curl_close($chAdd);
+    $de_line_mas= json_decode($result);
+    $count_line_mas = count($de_line_mas);
 ?>
 
-<form>
+<form action="Atest_select_member_filter.php" method="GET">
     <select name="line_id">
         <option value="">line@</option>
         <?php $i=0;
