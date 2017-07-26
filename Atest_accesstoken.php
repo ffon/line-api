1f5ea1f45aee2c5b531a6b4a8c470553cf7c74fe
ffon <poty1900@hotmail.com>
2017-07-26 15:34:19 +0700

    Update Atest_accesstoken.php

--- Atest_accesstoken.php
+++ Atest_accesstoken.php
@@ -1,8 +1,8 @@
 <html>
-  1<br>
+  1
 <?php 
     $line_id = $_GET['line_id'];
-    var_dump($id);
+    var_dump($line_id);
     $chAdd = curl_init();
     curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/get_line_master');
     curl_setopt($chAdd, CURLOPT_CUSTOMREQUEST, 'GET');
@@ -21,18 +21,17 @@
         if($de_line_mas[$i]->id == $line_id){
               $token=$de_line_mas[$i]->access_token;
         }
-        $i++;
-        echo $de_line_mas->line_name;
-        echo $token;
-      
+        $i++; 
     }
-
+    echo $de_line_mas[$i]->line_name;
+    echo $token;
     
 ?>
+ 
 <form action="Atest_select_member_filter.php" method="GET">
 <input type="hidden" value="<?php echo $line_id;?>" name="line_id"  />
 <input type="hidden" value="<?php echo $token;?>" name="access_token"  />
-<botton type="submit" name="submit"></botton>
+<botton type="submit" name="submit">submit</botton>
 </form>
 
 
