6be5cff6514ac0a133381e99290a545de7462339
ffon <poty1900@hotmail.com>
2017-07-26 13:35:03 +0700

    Update Atest_select_filter_lineadd.php

--- Atest_select_filter_lineadd.php
+++ Atest_select_filter_lineadd.php
@@ -1,5 +1,5 @@
 <html>
-  1
+  0
 <?php 
   $chAdd = curl_init();
   curl_setopt($chAdd, CURLOPT_URL, 'http://uat.dxplace.com/dxtms/get_line_master');
@@ -25,7 +25,8 @@
       <option value="">Choose line@</option>
       <?php $i=0; while($i!=$count_line_mas){ ?>
         <option type="text"  value="<?php $id_line=$de_line_mas[$i]->id; echo $id_line;  ?>"> <?php echo $id_line; echo " "; echo $de_line_mas[$i]->line_name ?></option> 
-          
+           <input type="hidden" value="<?php echo $de_line_mas[$id_line]->access_token ?>" name="access_token" /> 
+
 <!--         <input type="hidden" value="<?php //echo $de_line_mas[$id_line]->access_token ?>" name="access_token" /> -->
       <?php $i++; ?>
       <?}?>
